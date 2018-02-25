66b72bf394ceaa4b9be69effe21de54ca9fd633f
EvgputWeb <EvgputWeb@yandex.ru>
2018-02-26 02:36:17 +0300

    Выполнено задание #2

--- task2.php
+++ task2.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Евгений
- * Date: 26.02.2018
- * Time: 1:20
- */
+$totalDrawings = 80;
+$byFeltpen = 23;
+$byPencil = 40;
+$byPaints = $totalDrawings - $byFeltpen - $byPencil;
+
+echo '<b>Задача:</b><br>';
+echo "На школьной выставке $totalDrawings рисунков. $byFeltpen из них выполнены<br>";
+echo "фломастерами, $byPencil карандашами, а остальные - красками. <br>";
+echo "Сколько рисунков, выполненных красками, на школьной выставке?<br>";
+echo '<br>';
+echo '<b>Решение:</b><br>';
+echo 'Количество рисунков, выполненных красками = <br>';
+echo 'Общее количество рисунков - Количество рисунков фломастерами - Количество рисунков карандашами<br>';
+echo '<br>';
+echo '<b>Ответ:</b><br>';
+echo $totalDrawings - $byFeltpen - $byPencil;
