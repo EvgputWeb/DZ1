38e5a626d1eebd4643abe7b3b14dd52c8c1cd725
EvgputWeb <EvgputWeb@yandex.ru>
2018-02-26 14:08:35 +0300

    Выполнено задание #6

--- task6.php
+++ task6.php
@@ -1,7 +1,38 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Евгений
- * Date: 26.02.2018
- * Time: 1:21
- */
+$bmw = [
+    'model' => 'X5',
+    'speed' => 120,
+    'doors' => 5,
+    'year' => '2015'
+];
+
+$toyota = [
+    'model' => 'RAV4',
+    'speed' => 130,
+    'doors' => 4,
+    'year' => '2016'
+];
+
+$opel = [
+    'model' => 'Zafira',
+    'speed' => 125,
+    'doors' => 4,
+    'year' => '2013'
+];
+
+$cars['bmw'] = $bmw;
+$cars['toyota'] = $toyota;
+$cars['opel'] = $opel;
+
+foreach ($cars as $key => $value) {
+    echo "CAR $key <br>";
+    foreach ($value as $v) {
+        echo $v . ' ';
+    }
+    echo '<br><br>';
+}
+
+echo '<pre>';
+echo '$cars:<br>';
+print_r($cars);
+echo '</pre>';
